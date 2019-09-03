9640953879e0c00e4d1dfac9eff8bf97a6535145
ViktorovIO <viktorov_igor_1993@mail.ru>
2019-09-03 19:04:02 +0500

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Laravel 5.8 CRUD Application - Test</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+        <style>
+            body {
+                margin: 10% auto;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
